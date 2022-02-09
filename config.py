c6348f278c596bca9f6199a5773177bcfb1bfd5e
mattdharkins <97219656+mattdharkins@users.noreply.github.com>
2022-02-09 09:58:56 -0600

    day_2

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config():
     """
@@ -9,6 +12,8 @@ class Config():
     the config variable if not done already.
 
     """
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_ENV = os.environ.get('FLASK_ENV') 
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'You will never guess.'
-    SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
